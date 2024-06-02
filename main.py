c05d4b1e77fa6d417bfd317995ca3cca6ff3f818
RayanDri <rayan.drissi@icloud.com>
2024-06-03 00:52:56 +0200

    Refactor graph management, optimize drone path, and solve vehicle routing problem

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import osmnx as ox
 import networkx as nx
 import numpy as np
-from python_tsp.exact import solve_tsp_dynamic_programming
-from vrpy import VehicleRoutingProblem
-from networkx import DiGraph
+from tqdm import tqdm
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+from concorde.tsp import TSPSolver
 
 class GraphManager:
     """Classe pour gérer le téléchargement et le chargement du graphe de la ville."""
@@ -27,8 +28,10 @@ class GraphManager:
         :return: Le graphe de la ville.
         """
         if os.path.exists(self.file_path):
+            print("Chargement du graphe depuis le fichier...")
             self.graph = ox.load_graphml(self.file_path)
         else:
+            print("Téléchargement du graphe...")
             self.graph = ox.graph_from_place(self.city_name, network_type='drive')
             ox.save_graphml(self.graph, self.file_path)
         return self.graph
@@ -51,7 +54,7 @@ def create_distance_matrix(graph):
     nodes = list(graph.nodes)
     num_nodes = len(nodes)
     distance_matrix = np.zeros((num_nodes, num_nodes))
-    for i, node_i in enumerate(nodes):
+    for i, node_i in enumerate(tqdm(nodes, desc="Calcul des distances")):
         for j, node_j in enumerate(nodes):
             if i != j:
                 try:
@@ -62,15 +65,16 @@ def create_distance_matrix(graph):
 
 def optimize_drone_path(distance_matrix, nodes):
     """
-    Optimiser le trajet du drone en utilisant TSP.
+    Optimiser le trajet du drone en utilisant Concorde TSP Solver.
 
     :param distance_matrix: Matrice des distances.
     :param nodes: Liste des nœuds.
     :return: Le chemin optimisé et la distance totale.
     """
-    permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-    drone_path = [nodes[i] for i in permutation]
-    return drone_path, distance
+    solver = TSPSolver.from_data(distance_matrix)
+    solution = solver.solve()
+    drone_path = [nodes[i] for i in solution.tour]
+    return drone_path, solution.optimal_value
 
 def create_vrp_graph(graph, snow_removal_nodes):
     """
@@ -80,29 +84,59 @@ def create_vrp_graph(graph, snow_removal_nodes):
     :param snow_removal_nodes: Liste des nœuds nécessitant un déneigement.
     :return: Graphe VRP.
     """
-    G = DiGraph()
-    for u in snow_removal_nodes:
+    G = nx.DiGraph()
+    for u in tqdm(snow_removal_nodes, desc="Création du graphe VRP"):
         for v in snow_removal_nodes:
             if u != v:
                 try:
                     cost = nx.shortest_path_length(graph, u, v, weight='length')
-                    G.add_edge(u, v, cost=cost)
+                    G.add_edge(u, v, weight=cost)
                 except nx.NetworkXNoPath:
                     continue
     return G
 
 def solve_vrp(G, load_capacity, duration):
     """
-    Résoudre le problème de routage des véhicules (VRP).
+    Résoudre le problème de routage des véhicules (VRP) en utilisant OR-Tools.
 
     :param G: Graphe VRP.
     :param load_capacity: Capacité de charge des véhicules.
     :param duration: Durée maximale des trajets.
     :return: Valeur optimale et routes optimisées.
     """
-    prob = VehicleRoutingProblem(G, load_capacity=load_capacity, duration=duration)
-    prob.solve()
-    return prob.best_value, prob.best_routes
+    manager = pywrapcp.RoutingIndexManager(len(G.nodes), 1, 0)
+    routing = pywrapcp.RoutingModel(manager)
+
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return int(G[from_node][to_node]['weight'])
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.time_limit.seconds = 30
+
+    solution = routing.SolveWithParameters(search_parameters)
+
+    if not solution:
+        return None, None
+
+    routes = []
+    index = routing.Start(0)
+    while not routing.IsEnd(index):
+        routes.append(manager.IndexToNode(index))
+        index = solution.Value(routing.NextVar(index))
+    routes.append(manager.IndexToNode(index))
+    
+    total_distance = solution.ObjectiveValue()
+    
+    return total_distance, routes
 
 def main():
     city_name = 'Montreal, Quebec, Canada'
@@ -115,14 +149,18 @@ def main():
     # Créer la matrice de distances
     nodes, distance_matrix = create_distance_matrix(graph)
 
+    # Optimiser le trajet du drone
     drone_path, distance = optimize_drone_path(distance_matrix, nodes)
     print("Chemin du drone :", drone_path)
     print("Distance totale pour le chemin du drone :", distance)
 
+    # Identifier les zones nécessitant un déneigement
     snow_removal_nodes = drone_path
 
+    # Créer le graphe VRP
     G = create_vrp_graph(graph, snow_removal_nodes)
 
+    # Résoudre le VRP
     best_value, best_routes = solve_vrp(G, load_capacity=10, duration=5)
     print("Valeur optimale pour le VRP :", best_value)
     print("Routes optimisées pour le VRP :", best_routes)
